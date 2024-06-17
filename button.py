2d91f5f439f94eb7ef0077a6c831bed74d31a963
Nauka <berdinauryzbek@gmail.com>
2024-06-18 01:13:53 +0500

    made button class that can check user click

---
+++ button.py
@@ -0,0 +1,25 @@
+from game_settings import *
+
+
+class Button:
+    def __init__(self, button, btn_size, btn_rect):
+        self.button = pygame.transform.scale(button, btn_size)  # btn img
+        self.rect = self.button.get_rect(bottomleft=btn_rect)
+        self.surface = pygame.display.get_surface()     # surface for button display
+        self.clicked = False
+
+    def run(self):
+        action = False
+        pos = pygame.mouse.get_pos()    # getting mouse position
+
+        # check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        self.surface.blit(self.button, self.rect)
+        return action   # if the button is pressed, returns True
