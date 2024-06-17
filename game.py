ba69cc3730be993e6249212c88f2c77930642077
Nauka <berdinauryzbek@gmail.com>
2024-06-18 03:28:36 +0500

    displayed an alert for user for 3 seconds

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
+import time
 from game_settings import *
 from sys import exit
 from button import Button
 from text import Text
+from alert import Alert
 
 
 class Game:
@@ -9,40 +11,40 @@ class Game:
         # size of the game
         self.window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         # title of the game
-        pygame.display.set_caption("BlackJack")
+        pygame.display.set_caption(GAME_TITLE)
 
         # icon of the game
-        self.game_icon = pygame.image.load('imgs/game_icon.png')
-        pygame.display.set_icon(self.game_icon)
+        pygame.display.set_icon(GAME_ICON)
 
         # background image
-        bg_image = pygame.image.load('imgs/game_desk.png')
-        self.bg_image = pygame.transform.scale(bg_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.bg_image = pygame.transform.scale(BG_IMAGE, (WINDOW_WIDTH, WINDOW_HEIGHT))
 
         # button to start the game
-        start_button = pygame.image.load('imgs/start_button.png')
-        self.start_button = Button(button=start_button, btn_size=(300, 300), btn_rect=(200, 670))
+        self.start_button = Button(button=START_BUTTON, btn_size=(300, 300), btn_rect=(200, 670))
 
         # button to exit the game
-        quit_button = pygame.image.load('imgs/quit_button.png')
-        self.quit_button = Button(button=quit_button, btn_size=(300, 300), btn_rect=(500, 670))
+        self.quit_button = Button(button=QUIT_BUTTON, btn_size=(300, 300), btn_rect=(500, 670))
 
-        # show game
-        self.show_elements = True
+        # show game menu
+        self.game_menu = True
 
         # game title on the screen
-        self.display_game_title = Text(text="BlackJack")
+        self.display_game_title = Text(text=GAME_TITLE)
 
         # game icon on the screen
-        self.display_game_icon = pygame.transform.scale(self.game_icon, (200, 150))
+        self.display_game_icon = pygame.transform.scale(GAME_ICON, (200, 150))
         self.icon_rect = self.display_game_icon.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+        # display alert for user
+        self.alert = Alert()
+        self.alert_start_time = None  # track when the alert starts
 
     def run(self):
         while True:
             # display game desk
             self.window.blit(self.bg_image, (0, 0))
 
-            if self.show_elements:
+            if self.game_menu:
 
                 # display title of the game
                 self.window.blit(self.display_game_title.surface, self.display_game_title.rect)
@@ -53,11 +55,19 @@ class Game:
                 # display start btn img
                 if self.start_button.run():
                     print('Game Start')
+                    self.alert_start_time = time.time()
 
                 # display quit btn img
                 if self.quit_button.run():
                     print('Game Over')
                     break
+
+                if self.alert_start_time:
+                    if time.time() - self.alert_start_time < 3:
+                        self.alert.run()
+                    else:
+                        self.alert_start_time = None
+                        self.game_menu = False
 
             pygame.display.update()
 
