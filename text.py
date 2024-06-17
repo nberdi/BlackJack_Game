de336c3b7f11dba01bc81d19159d9263cf29af97
Nauka <berdinauryzbek@gmail.com>
2024-06-18 02:38:57 +0500

    added parameter pos

--- text.py
+++ text.py
@@ -2,7 +2,7 @@ from game_settings import *
 
 
 class Text:
-    def __init__(self, text, color=(128, 0, 0), size=120):
+    def __init__(self, text, color=(128, 0, 0), size=120, pos=(WINDOW_WIDTH / 2, 75)):
         self.font = pygame.font.SysFont('bald', size)  # font and size
         self.surface = self.font.render(text, True, color)
-        self.rect = self.surface.get_rect(center=(WINDOW_WIDTH / 2, 200))
+        self.rect = self.surface.get_rect(center=pos)
