6e5e4354464a46e0fd766379e9395da6dd414a1f
Nauka <berdinauryzbek@gmail.com>
2024-06-18 02:39:40 +0500

    modified game setting by loading images

--- game_settings.py
+++ game_settings.py
@@ -3,3 +3,22 @@ pygame.init()
 
 # game window
 WINDOW_WIDTH, WINDOW_HEIGHT = 1000, 600
+
+# game title
+GAME_TITLE = "BlackJack"
+
+# game icon
+GAME_ICON = pygame.image.load('imgs/game_icon.png')
+
+# game bg
+BG_IMAGE = pygame.image.load('imgs/game_desk.png')
+
+# start btn img
+START_BUTTON = pygame.image.load('imgs/start_button.png')
+
+# quit btn img
+QUIT_BUTTON = pygame.image.load('imgs/quit_button.png')
+
+# alert size
+ALERT_WIDTH = 500
+ALERT_HEIGHT = 300
