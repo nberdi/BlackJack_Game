716d04d330f3fb786bff30bd6c4d82101429c10f
Nauka <berdinauryzbek@gmail.com>
2024-06-20 17:02:14 +0500

    added surface, different result texts and their functions to display

--- display_cards.py
+++ display_cards.py
@@ -7,6 +7,10 @@ from text import Text
 
 class Cards:
     def __init__(self):
+        self.surface = pygame.surface.Surface((CARD_WIDTH, CARD_HEIGHT))
+        self.rect = self.surface.get_rect(center=(420, 70))
+        self.display_surface = pygame.display.get_surface()
+
         # get random two cards from a dict of the list; check if there is an ace
         self.user_cards = self.is_eleven(self.random_cards(all_cards))
         # count user total score
@@ -18,10 +22,16 @@ class Cards:
         # get random two cards from a dict of the list; check if there is an ace
         self.dealer_cards = self.is_eleven(self.random_cards(all_cards))
         # count dealer total score
-        self.dealer_current_score = self.sum_card_values(self.dealer_cards)
+        self.dealer_current_score = self.dealer_cards[1]
         # load dealer card imgs
         self.dealer_first_card = Button(button=self.dealer_cards[0], btn_size=(130, 130), btn_rect=(550, 300))
         self.dealer_second_card = Button(button=self.dealer_cards[2], btn_size=(130, 130), btn_rect=(700, 300))
+
+        # results
+        self.blackjack_win = Text(text="You win with a 'Blackjack'", size=60, pos=(400, 70))
+        self.user_win = Text(text="You Win!", size=60, pos=(400, 70))
+        self.dealer_win = Text(text="Dealer Wins!", size=60, pos=(400, 70))
+        self.draw = Text(text="Draw!", size=60, pos=(400, 70))
 
     def display_user_score(self, user_score):
         display_user_score = Text(text=f"Your score is: {user_score}", color=(0, 0, 0), size=35)
@@ -67,3 +77,12 @@ class Cards:
             if isinstance(i, int):
                 summed_card_values += i
         return summed_card_values
+
+    def display_user_win(self):
+        self.display_surface.blit(self.user_win.surface, self.user_win.rect)
+
+    def display_dealer_win(self):
+        self.display_surface.blit(self.dealer_win.surface, self.dealer_win.rect)
+
+    def display_draw(self):
+        self.display_surface.blit(self.draw.surface, self.draw.rect)
